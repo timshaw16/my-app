552e85c740473d73f04a1eda7f1253aa634760fe
Tim Shaw <TShaw2@golin.com>
2017-09-05 12:09:08 +0100

    first commit

---
+++ client/app/scripts/app.js
@@ -0,0 +1,42 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name clientApp
+ * @description
+ * # clientApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('clientApp', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch'
+  ])
+  .config(function ($routeProvider, $locationProvider) {
+    $locationProvider.html5Mode(true); // <-- ADD THIS
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .when('/about', {
+        templateUrl: 'views/about.html',
+        controller: 'AboutCtrl',
+        controllerAs: 'about'
+      })
+      .when('/kitchen', {
+        templateUrl: 'views/kitchen.html',
+        controller: 'KitchenCtrl',
+        controllerAs: 'kitchen'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
